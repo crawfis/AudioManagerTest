1fc3a37f957b5ba9e00985a11072987dd1b66e2a
Roger Crawfis <crawfis.3@osu.edu>
2022-01-03 06:19:27 -0500

    Pooled AudioSource testing

---
+++ Assets/Scripts/Audio/RegisterAudioFactoryPrefabPooled.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace GTMY.Audio.Scripts
+{
+    /// <summary>
+    /// MonoBehaviour (primarily for testing) to create an IAudioFactory using the GameObject
+    /// that contains the specified AudioSource.
+    /// </summary>
+    public class RegisterAudioFactoryPrefabPooled : MonoBehaviour
+    {
+        [SerializeField]
+        private string audioType;
+        [SerializeField]
+        private GameObject audioPrefab;
+
+        private void Awake()
+        {
+            //foreach(var tuple in audioPrefabs)
+            {
+                var factory = new AudioFactoryPooled(this, audioPrefab);
+                AudioFactoryRegistry.Instance.RegisterAudioFactory(audioType, factory);
+            }
+        }
+    }
+}
